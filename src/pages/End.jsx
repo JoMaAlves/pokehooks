5ffa1c5e4fdbc249f8dccf5584725d84ff0f0a06
Joao Marcelo Alves <jmarsdalves@gmail.com>
2025-01-03 12:56:58 -0300

    Updated End Page

--- src/pages/End.jsx
+++ src/pages/End.jsx
@@ -1,5 +1,13 @@
+import EndImg from '../assets/end.png';
+
 const End = () => {
-  return <div>End</div>;
+  return (
+    <div className="align-items-center d-flex flex-column">
+      <img className="mb-3" src={EndImg} width={700} />
+
+      <h2 className="text-center">Thank you Everyone!</h2>
+    </div>
+  );
 };
 
 export default End;
